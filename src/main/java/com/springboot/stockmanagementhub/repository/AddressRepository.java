e9d615247e129e7cd8765d1c924fda91e56f10c6
Moronkola Adijat <horythorkeh@gmail.com>
2024-08-24 16:26:36 +0100

    role & address repository class

---
+++ src/main/java/com/springboot/stockmanagementhub/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.stockmanagementhub.repository;
+
+import com.springboot.stockmanagementhub.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
