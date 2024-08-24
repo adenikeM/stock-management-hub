c988bc1476a3e738a73b3a88b2646ea37d1d1f1c
Moronkola Adijat <horythorkeh@gmail.com>
2024-08-24 15:09:39 +0100

    Initial commit

---
+++ src/main/java/com/springboot/stockmanagementhub/StockManagementHubApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.stockmanagementhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/stockmanagementhub/StockManagementHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.stockmanagementhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
